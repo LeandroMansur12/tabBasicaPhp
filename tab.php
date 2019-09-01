a5d550d07277c85ee66b48dfb8d9ed44542970db
Leandro Mansur <leandroapmansur@gmail.com>
2019-09-01 10:29:09 -0300

    basico tabuada

---
+++ tab.php
@@ -0,0 +1,27 @@
+<?php 
+
+$num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+	
+
+ 	
+	echo PHP_EOL;
+
+
+	
+
+	for( $x=0; $x<10; $x++){
+
+
+
+		for( $i = 1; $i<=10; $i++) {
+			
+
+
+
+		echo PHP_EOL . "$i x $num[$x] =" .( $i * $num[$x]) . PHP_EOL;
+
+
+}   
+
+}
---
+++ tabuada.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+$num = 1;
+$num2=3;
+
+echo "tabuada do $num2".PHP_EOL;
+
+while( $num<=10 ){
+	
+	echo "$num2 x $num = " . ($num * $num2) . PHP_EOL;
+	$num++;
+
+}
+
+
+?>
